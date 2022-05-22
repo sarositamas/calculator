3c42fa73b98422270a6b85d9e32aa4fd2631fd00
sarositamas <decodedcode@protonmail.com>
2022-05-22 12:47:40 +0100

    Functions for the basic operations has been created

---
+++ script.js
@@ -0,0 +1,17 @@
+//The functions for basic operations of the calculator
+
+const add = function(a, b) {
+	return a + b;
+};
+
+const subtract = function(a, b) {
+	return a - b;
+};
+
+const multiply = function(a, b) {
+    return (a * b);
+};
+
+const divide = function(a, b){
+    return a / b;
+};
